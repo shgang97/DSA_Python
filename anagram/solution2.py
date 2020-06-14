586a0f717692347dae8f90a6485e075eda3daf0c
shgang <shgang97@163.com>
2020-06-15 03:11:49 +0800

    add anagram

---
+++ anagram/solution2.py
@@ -0,0 +1,17 @@
+# 排序比较--O(n*logn)
+def anagramSolution2(s1, s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+    alist1.sort()
+    alist2.sort()
+    pos = 0
+    matches = True
+    while pos < len(s1) and matches:
+        if alist1[pos] == alist2[pos]:
+            pos += 1
+        else:
+            matches = False
+    return matches
+
+
+print(anagramSolution2('python', 'typhon'))
