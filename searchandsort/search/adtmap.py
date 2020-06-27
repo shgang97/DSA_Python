90b1485fe3b462c3ad03e1aab9ab6d7424974dc5
shgang <shgang97@163.com>
2020-06-27 21:31:49 +0800

    add adtmap

---
+++ searchandsort/search/adtmap.py
@@ -0,0 +1,51 @@
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def hashfunction(self, key):
+        return key % self.size
+
+    def rehash(self, oldhash):
+        return (oldhash + 1) % self.size
+
+    def put(self, key, data):
+        hashvalue = self.hashfunction(key)
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+        else:
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data
+            else:
+                nextslot = self.rehash(hashvalue)
+                while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                    nextslot = self.rehash(nextslot)
+                if self.slots[nextslot] == None:
+                    self.slots[nextslot] = key
+                    self.data[nextslot] = data
+                else:
+                    self.data[nextslot] = data
+
+    def get(self, key):
+        startslot = self.hashfunction(key)
+        data = None
+        stop = False
+        found = False
+        pos = startslot
+        while self.slots[pos] != None and not found and not stop:
+            if self.slots[pos] == key:
+                found = True
+                data = self.data[pos]
+            else:
+                pos = self.rehash(pos)
+                if pos == startslot:
+                    stop = True
+        return data
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
